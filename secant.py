5d7b63a28659b12a5b12e363ca43179d934356f8
LeQuXi <59503592+LeQuXi@users.noreply.github.com>
2021-10-06 13:59:26 +0200

    Update secant.py
    modified for finding multiple roots

--- secant.py
+++ secant.py
@@ -1,10 +1,18 @@
+import math
 import numpy as np;
 import matplotlib.pyplot as plt
-
-
 def func(x):
-    return 0.85 * x ** 4 - 9.92 * x ** 3 + 40.02 * x ** 2 - 64.68 * x + 34.25
-
+    return 0.85 * x ** 4 - 9.92 * x ** 3 + 40.02 * x ** 2 - 64.68 * x + 34.25 
+def SearchforRoots(func,a1,a2):
+    step=0.01
+    x1 = a1; f1 = func(a1)
+    x2 = a1 + step; f2 = func(x2)
+    while f1*f2 > 0.0:
+        if x1 >= a2:
+            return None,None
+        x1 = x2; f1 = f2
+        x2 = x1 + step; f2 = func(x2)
+    return x1,x2
 
 def Secant(func, x0, x1):
     e = 1e-10
@@ -18,7 +26,7 @@ def Secant(func, x0, x1):
             break
 
         x2 = x0 - (x1 - x0) * func(x0) / (func(x1) - func(x0))
-        print('Iteration-%d, x2 = %0.4f and f(x2) = %0.6f' % (counter, x2, func(x2)))
+        #print('Iteration-%d, x2 = %0.4f and f(x2) = %0.6f' % (counter, x2, func(x2)))
         x0 = x1
         x1 = x2
         counter = counter + 1
@@ -31,10 +39,21 @@ def Secant(func, x0, x1):
     print('\n Exact root is: %0.8f' % x2)
     return x2
 
-
-x1 = Secant(func, 0, 5)
-print('\n x = {0:.20f} f(x) = {1:.20f}'.format(x1, func(x1)))
-x = np.linspace(0, 7, 100)
+def TotalRoots(func, a1, a2, eps=1e-6):
+    print ('The roots on the interval [%f, %f] are:' % (a1,a2))
+    while 1:
+        x1,x2 = SearchforRoots(func,a1,a2)
+        if x1 != None:
+            a1 = x2
+            root = Secant(func,x1,x2)
+            if root != None:
+                pass
+                print (round(root,-int(math.log(eps, 10))))
+        else:
+            print ('\nDone')
+            break
+TotalRoots(func, 0, 5)
+x = np.linspace(0, 5, 100)
 y = func(x)
 plt.plot(x, y)
 plt.grid()
